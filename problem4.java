9c5887ffb47721bd3a6b75a440364a1008b4312b
jeevananthams3113 <santhoshkumar.s2023cse@sece.ac.in>
2026-06-16 18:07:56 +0530

    problem 4

---
+++ problem4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class problem4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        for(int i=0;i<s1.length();i++){
+            boolean flag=true;
+            for(int j=0;j<s1.length();j++){
+                if(i==j) continue;
+                char a=s1.charAt(i);
+                char b=s1.charAt(j);
+                if(a==b){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
