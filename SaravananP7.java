d5f639154547a0c5365a8d80d5bd6a7a22ec0cdb
Saravanank2005 <saravanan.k2023it@sece.ac.in>
2026-06-17 09:06:22 +0530

    Check if two strings are anagrams of each other

---
+++ SaravananP7.java
@@ -0,0 +1,16 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SaravananP7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        char[] a1=s1.toLowerCase().toCharArray();
+        char[] a2=s2.toLowerCase().toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        System.out.println(Arrays.equals(a1, a2));
+    }
+}
