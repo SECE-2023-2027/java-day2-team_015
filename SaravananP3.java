b5ed107ad402bf27127305378faf32c2f29622b2
Saravanank2005 <saravanan.k2023it@sece.ac.in>
2026-06-17 09:06:12 +0530

    Concatenate two strings except their first character

---
+++ SaravananP3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SaravananP3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        String ans=s1.substring(1)+s2.substring(1);
+        System.out.println(ans);
+    }
+}
