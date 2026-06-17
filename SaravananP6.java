4d3fe85248f80ad7750d544f021483a13a4c76bc
Saravanank2005 <saravanan.k2023it@sece.ac.in>
2026-06-17 09:06:20 +0530

    Remove all occurrences of a specified value from an array

---
+++ SaravananP6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SaravananP6 {
+    public static void main(String[] args) {
+        int[] arr={1,4,6,7,6,2};
+        int val=6;
+        int i=0;
+        for(int a:arr){
+            if(a!=val){
+                arr[i]=a;
+                i++;
+            }
+        }
+        for(int j=0;j<i;j++)
+            System.out.print(arr[j]+" ");
+    }
+}
